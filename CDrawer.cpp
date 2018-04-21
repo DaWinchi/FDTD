ae89614310daa60236342697bcf0739403b8b0e4
prutskov <lehaprutskov@gmail.com>
2018-04-21 15:06:58 +0300

    Выводится поверхность cos(xy) в лоб

--- CDrawer.cpp
+++ CDrawer.cpp
@@ -13,11 +13,13 @@ CDrawer::~CDrawer()
 
 void CDrawer::Draw()
 {
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINES);
+
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-10, 10, -10, 10, -10, 10);
 
-	glTranslated(0.0f, 0.0f, -1.0f);
+	glTranslated(0.0f, 0.0f, 0.0f);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	
@@ -29,45 +31,70 @@ void CDrawer::Draw()
 	glScaled(*zoom, *zoom, *zoom);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_QUADS);//
 	glBegin(GL_QUADS);
-	//передн€€
-	glColor3d(255, 0, 0);
-	glVertex3d(-5.0, -5.0, 5.0); //1
-	glVertex3d(5.0, -5.0, 5.0);  //2
-	glVertex3d(5.0, 5.0, 5.0);//3
-	glVertex3d(-5.0, 5.0, 5.0);//4
+	////передн€€
+	//glColor3d(255, 0, 0);
+	//glVertex3d(-5.0, -5.0, 5.0); //1
+	//glVertex3d(5.0, -5.0, 5.0);  //2
+	//glVertex3d(5.0, 5.0, 5.0);//3
+	//glVertex3d(-5.0, 5.0, 5.0);//4
 
-													   //верх
-	glColor3d(255, 255, 0);
-	glVertex3d(-5.0, 5.0, 5.0); //1
-	glVertex3d(5.0, 5.0, 5.0); //2
-	glVertex3d(5.0, 5.0, -5.0); //3
-	glVertex3d(-5.0, 5.0, -5.0); //4
-													 //задн€€
-	glColor3d(255, 0, 0);
-	glVertex3d(5.0, -5.0, -5.0); //1
-	glVertex3d(-5.0, -5.0, -5.0); //2
-	 glVertex3d(-5.0, 5.0, -5.0); //3
-	 glVertex3d(5.0, 5.0, -5.0); //4
+	//												   //верх
+	//glColor3d(255, 255, 0);
+	//glVertex3d(-5.0, 5.0, 5.0); //1
+	//glVertex3d(5.0, 5.0, 5.0); //2
+	//glVertex3d(5.0, 5.0, -5.0); //3
+	//glVertex3d(-5.0, 5.0, -5.0); //4
+	//												 //задн€€
+	//glColor3d(255, 0, 0);
+	//glVertex3d(5.0, -5.0, -5.0); //1
+	//glVertex3d(-5.0, -5.0, -5.0); //2
+	// glVertex3d(-5.0, 5.0, -5.0); //3
+	// glVertex3d(5.0, 5.0, -5.0); //4
 
-													//низ
-	glColor3d(0, 0, 255);
-	glVertex3d(5.0, -5.0, 5.0); //1
-	glVertex3d(-5.0, -5.0, 5.0); //2
-	 glVertex3d(-5.0, -5.0, -5.0); //3
-	 glVertex3d(5.0, -5.0, -5.0); //4
-													 //право
-	glColor3d(0, 255, 0);
-	glVertex3d(5.0, -5.0, 5.0); //1
-	 glVertex3d(5.0, -5.0, -5.0); //2
-	glVertex3d(5.0, 5.0, -5.0); //3
-	glVertex3d(5.0, 5.0, 5.0); //4
-														 //лево
-	glColor3d(255, 0, 255); 
-	glVertex3d(-5.0, -5.0, -5.0); //1
-	glVertex3d(-5.0, -5.0, 5.0); //2
-	glVertex3d(-5.0, 5.0, 5.0); //3
-	glVertex3d(-5.0, 5.0, -5.0); //4
+	//												//низ
+	//glColor3d(0, 0, 255);
+	//glVertex3d(5.0, -5.0, 5.0); //1
+	//glVertex3d(-5.0, -5.0, 5.0); //2
+	// glVertex3d(-5.0, -5.0, -5.0); //3
+	// glVertex3d(5.0, -5.0, -5.0); //4
+	//												 //право
+	//glColor3d(0, 255, 0);
+	//glVertex3d(5.0, -5.0, 5.0); //1
+	// glVertex3d(5.0, -5.0, -5.0); //2
+	//glVertex3d(5.0, 5.0, -5.0); //3
+	//glVertex3d(5.0, 5.0, 5.0); //4
+	//													 //лево
+	//glColor3d(255, 0, 255); 
+	//glVertex3d(-5.0, -5.0, -5.0); //1
+	//glVertex3d(-5.0, -5.0, 5.0); //2
+	//glVertex3d(-5.0, 5.0, 5.0); //3
+	//glVertex3d(-5.0, 5.0, -5.0); //4
+	for (int i = 0; i < points[0].size(); i++)
+	{
+		for (int j = 0; j < points[0][i].size(); j++)
+		{
+			glColor3d(1.0f, 1.0f, 1.0f);
+			int iright = i + 1;
+			int jup = j + 1;
+			if (iright == points[0].size())
+			{
+				iright -=2 ;
+			}
+			if (jup == points[0][i].size())
+			{
+				jup -= 2;
+			}
+			if (points[0][i][j].z<0) glColor3d(0, 0, 255); else glColor3d(255, 0, 0);
+			glVertex3d(points[0][i][j].x , points[0][i][j].y,points[0][i][j].z);
+			if (points[0][iright][j].z<0) glColor3d(0, 0, 255); else glColor3d(255, 0, 0);
+			glVertex3d(points[0][iright][j].x, points[0][iright][j].y, points[0][iright][j].z);
+			if (points[0][iright][jup].z<0) glColor3d(0, 0, 255); else glColor3d(255, 0, 0);
+			glVertex3d(points[0][iright][jup].x, points[0][iright][jup].y, points[0][iright][jup].z);
+			if (points[0][i][jup].z<0) glColor3d(0, 0, 255); else glColor3d(255, 0, 0);
+			glVertex3d(points[0][i][jup].x, points[0][i][jup].y, points[0][i][jup].z);
+		}
+	}
 	glEnd();
-
+	glFinish();
 	SwapBuffers(wglGetCurrentDC());
 }
--- CDrawer.h
+++ CDrawer.h
@@ -2,6 +2,12 @@
 #include <gl/GLU.h>
 #include <gl/GL.h>
 #include "glut.h"
+#include <vector>
+
+struct Point3D
+{
+	double x, y, z;
+};
 
 class CDrawer
 {
@@ -11,5 +17,6 @@ public:
 
 	double *angleX, *angleY, *zoom;
 	void Draw();
+	std::vector<std::vector<Point3D>> *points;
 };
 
