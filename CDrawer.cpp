1964af8b8d53e1b9e1e49a1b4f49fae725e08473
prutskov <lehaprutskov@gmail.com>
2018-04-21 12:18:35 +0300

    Сделан поворот

--- CDrawer.cpp
+++ CDrawer.cpp
@@ -29,37 +29,43 @@ void CDrawer::Draw()
 	glPolygonMode(GL_FRONT_AND_BACK, GL_QUADS);//
 	glBegin(GL_QUADS);
 	//передн€€
-	glColor3d(132, 112, 255); glVertex3d(-5.0, -5.0, 5.0); //1
-	glColor3d(255, 255, 0); glVertex3d(5.0, -5.0, 5.0);  //2
-	glColor3d(238, 233, 233); glVertex3d(5.0, 5.0, 5.0);//3
-	glColor3d(0, 245, 255); glVertex3d(-5.0, 5.0, 5.0);//4
+	glColor3d(255, 0, 0);
+	glVertex3d(-5.0, -5.0, 5.0); //1
+	glVertex3d(5.0, -5.0, 5.0);  //2
+	glVertex3d(5.0, 5.0, 5.0);//3
+	glVertex3d(-5.0, 5.0, 5.0);//4
 
 													   //верх
-	glColor3d(0, 245, 255); glVertex3d(-5.0, 5.0, 5.0); //1
-	glColor3d(238, 233, 233); glVertex3d(5.0, 5.0, 5.0); //2
-	glColor3d(0, 0, 1); glVertex3d(5.0, 5.0, -5.0); //3
-	glColor3d(0, 0, 1); glVertex3d(-5.0, 5.0, -5.0); //4
+	glColor3d(255, 255, 0);
+	glVertex3d(-5.0, 5.0, 5.0); //1
+	glVertex3d(5.0, 5.0, 5.0); //2
+	glVertex3d(5.0, 5.0, -5.0); //3
+	glVertex3d(-5.0, 5.0, -5.0); //4
 													 //задн€€
-	glColor3d(0, 0, 1); glVertex3d(5.0, -5.0, -5.0); //1
-	glColor3d(0, 0, 1); glVertex3d(-5.0, -5.0, -5.0); //2
-	glColor3d(1, 1, 0); glVertex3d(-5.0, 5.0, -5.0); //3
-	glColor3d(0, 0, 1); glVertex3d(5.0, 5.0, -5.0); //4
+	glColor3d(255, 0, 0);
+	glVertex3d(5.0, -5.0, -5.0); //1
+	glVertex3d(-5.0, -5.0, -5.0); //2
+	 glVertex3d(-5.0, 5.0, -5.0); //3
+	 glVertex3d(5.0, 5.0, -5.0); //4
 
 													//низ
-	glColor3d(1, 0, 0); glVertex3d(5.0, -5.0, 5.0); //1
-	glColor3d(0, 0, 1); glVertex3d(-5.0, -5.0, 5.0); //2
-	glColor3d(0, 0, 1); glVertex3d(-5.0, -5.0, -5.0); //3
-	glColor3d(0, 0, 1); glVertex3d(5.0, -5.0, -5.0); //4
+	glColor3d(0, 0, 255);
+	glVertex3d(5.0, -5.0, 5.0); //1
+	glVertex3d(-5.0, -5.0, 5.0); //2
+	 glVertex3d(-5.0, -5.0, -5.0); //3
+	 glVertex3d(5.0, -5.0, -5.0); //4
 													 //право
-	glColor3d(255, 255, 0); glVertex3d(5.0, -5.0, 5.0); //1
-	glColor3d(0, 0, 1); glVertex3d(5.0, -5.0, -5.0); //2
-	glColor3d(0, 0, 1); glVertex3d(5.0, 5.0, -5.0); //3
-	glColor3d(238, 233, 233); glVertex3d(5.0, 5.0, 5.0); //4
+	glColor3d(0, 255, 0);
+	glVertex3d(5.0, -5.0, 5.0); //1
+	 glVertex3d(5.0, -5.0, -5.0); //2
+	glVertex3d(5.0, 5.0, -5.0); //3
+	glVertex3d(5.0, 5.0, 5.0); //4
 														 //лево
-	glColor3d(0, 0, 1); glVertex3d(-5.0, -5.0, -5.0); //1
-	glColor3d(132, 112, 255); glVertex3d(-5.0, -5.0, 5.0); //2
-	glColor3d(0, 245, 255); glVertex3d(-5.0, 5.0, 5.0); //3
-	glColor3d(0, 0, 1); glVertex3d(-5.0, 5.0, -5.0); //4
+	glColor3d(255, 0, 255); 
+	glVertex3d(-5.0, -5.0, -5.0); //1
+	glVertex3d(-5.0, -5.0, 5.0); //2
+	glVertex3d(-5.0, 5.0, 5.0); //3
+	glVertex3d(-5.0, 5.0, -5.0); //4
 	glEnd();
 
 	SwapBuffers(wglGetCurrentDC());
